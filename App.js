d19839884040d27b7809c245c05968092a8bea82
matthew29tang <matthew29tang@gmail.com>
2018-06-16 02:18:01 -0700

    Add text boxes to UI

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { List, TextareaItem } from 'antd-mobile-rn';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: '',
+      budget: '',
+      interest: '',
+      taste: '',
+      start_date:'',
+      end_date:''
+    };
+  }
+
+  changeLocation = (val) => { this.setState({ location:val }); };
+  changeBudget = (val) => { this.setState({ budget:val }); };
+  changeInterest = (val) => { this.setState({ interest:val }); };
+  changeTaste = (val) => { this.setState({ taste:val }); };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>BonVoyApp!</Text>
+        <TextareaItem value={this.state.location} placeholder = "Location" onChange={this.changeLocation} style={styles.box} />
+        <TextareaItem value={this.state.budget} placeholder = "Budget" onChange={this.changeBudget} style={styles.box} />
+        <TextareaItem value={this.state.interest} placeholder = "Interest" onChange={this.changeInterest} style={styles.box} />
+        <TextareaItem value={this.state.taste} placeholder = "Taste Preference" onChange={this.changeTaste} style={styles.box} />
       </View>
     );
   }
@@ -18,6 +39,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 50
   },
+  box: {
+    width: 250
+  }
 });
